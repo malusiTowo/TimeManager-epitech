85e621cf31fced6acaeb2fc0491058bf4e9a2527
Malusi Towo <malusitowo@gmail.com>
2020-10-23 15:33:28 +0200

    fix server config

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -19,7 +19,8 @@ config :api, Api.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :api, ApiWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: {:system, "PORT"}]
+  # http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
