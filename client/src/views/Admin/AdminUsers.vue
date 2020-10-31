94292dcefcaf74d5b8858c36175427f6d269b3c9
Sandra-I <sandra.ibrahim@hotmail.fr>
2020-10-31 20:49:28 +0100

    admin users page background color

--- client/src/views/Admin/AdminUsers.vue
+++ client/src/views/Admin/AdminUsers.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    <base-header class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-success">
-    </base-header>
+    <div class="header bg-yellow py-7 py-lg-8 pt-lg-9">
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg
+          x="0"
+          y="0"
+          viewBox="0 0 2560 100"
+          preserveAspectRatio="none"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polygon
+            class="fill-default"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
+        </svg>
+      </div>
+    </div>
 
     <b-container fluid class="mt--7">
       <b-card>
