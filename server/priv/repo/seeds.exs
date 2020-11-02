0c00e6211cd4c4ecb5ac437c4d8570faec3db925
Malusi Towo <malusitowo@gmail.com>
2020-11-02 13:33:34 +0100

    add initial admin user in database

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+alias Api.Repo
+alias Api.Users.User
+alias Api.Users
+
+user_params = %{
+  username: "admin",
+  email: "admin@email.com",
+  password: "Password1",
+}
+
+update_params = %{
+  role: "admin"
+}
+changeset = User.registration_changeset(%User{}, user_params)
+
+ with {:ok, %User{} = user} <- Repo.insert(changeset) do
+  Users.update_user(user, update_params)
+end
