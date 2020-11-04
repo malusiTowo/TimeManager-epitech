02b65a1e7a97e89f5b4391def25c28440722bb5f
francisco-eptck <francisco.soares-dos-santos@epitech.eu>
2020-11-04 17:47:36 +0100

    implementation Tests CI Team Context

--- server/test/api/team_context_test.exs
+++ server/test/api/team_context_test.exs
@@ -75,40 +75,41 @@ defmodule Api.TeamContextTest do
 
     def team_user_fixture(attrs \\ %{}) do
 
-      # team_attrs =  %{name: "some name", description: "some desc"}
+      team_attrs =  %{name: "some name", description: "some desc"}
 
-      # rand = Enum.random(1_000..9_999)
-      # emailGen = "some@email.com#{rand}"
-      # user_attrs =  %{email: emailGen, username: "some username", password: "Password1", role: "employee"}
+      rand = Enum.random(1_000..9_999)
+      emailGen = "some@email.com#{rand}"
+      user_attrs =  %{email: emailGen, username: "some username", password: "Password1", role: "employee"}
 
-      # case Users.create_user(user_attrs) do
-      #   {:ok, user} ->
-      #     case TeamContext.create_team(team_attrs) do
-      #       {:ok, team} ->
-      #         team_user = TeamContext.create_team_user(%{role: "some role", team: team.id, user: user.id})
-      #     end
-      # end
+      case Users.create_user(user_attrs) do
+        {:ok, user} ->
+          case TeamContext.create_team(team_attrs) do
+            {:ok, team} ->
+              team_user = TeamContext.create_team_user(%{role: "some role", team: team.id, user: user.id})
+          end
+      end
 
-      attrs = %{role: "some role", team: 1, user: 1})
+      # attrs = %{role: "some role", team: 1, user: 1}
 
-      {:ok, team_user} =
-        attrs
-        |> Enum.into(attrs)
-        |> TeamContext.create_team_user()
+      # {:ok, team_user} =
+      #   attrs
+      #   |> Enum.into(attrs)
+      #   |> TeamContext.create_team_user()
+
     end
 
     test "list_team_users/0 returns all team_users" do
-      team_user = team_user_fixture()
+      {:ok, %TeamUser{} = team_user} = team_user_fixture()
       assert TeamContext.list_team_users() == [team_user]
     end
 
     test "get_team_user!/1 returns the team_user with given id" do
-      team_user = team_user_fixture()
+      assert {:ok, %TeamUser{} = team_user} = team_user_fixture()
       assert TeamContext.get_team_user!(team_user.id) == team_user
     end
 
     test "create_team_user/1 with valid data creates a team_user" do
-      team_user = team_user_fixture()
+      {:ok, %TeamUser{} = team_user} = team_user_fixture()
       assert team_user.role == @valid_attrs[:role]
     end
 
@@ -117,25 +118,24 @@ defmodule Api.TeamContextTest do
     end
 
     test "update_team_user/2 with valid data updates the team_user" do
-      team_user = team_user_fixture()
+      {:ok, %TeamUser{} = team_user} = team_user_fixture()
       assert {:ok, %TeamUser{} = team_user} = TeamContext.update_team_user(team_user, @update_attrs)
       assert team_user.role == @update_attrs[:role]
     end
 
     test "update_team_user/2 with invalid data returns error changeset" do
-      team_user = team_user_fixture()
-      assert {:error, %Ecto.Changeset{}} = TeamContext.update_team_user(team_user, @invalid_attrs)
-      assert team_user == TeamContext.get_team_user!(team_user.id)
+      {:ok, %TeamUser{} = team_user} = team_user_fixture()
+      assert_raise Postgrex.Error, fn -> TeamContext.update_team_user(team_user, @invalid_attrs) end
     end
 
     test "delete_team_user/1 deletes the team_user" do
-      team_user = team_user_fixture()
+      {:ok, %TeamUser{} = team_user} = team_user_fixture()
       assert {:ok, %TeamUser{}} = TeamContext.delete_team_user(team_user)
       assert_raise Ecto.NoResultsError, fn -> TeamContext.get_team_user!(team_user.id) end
     end
 
     test "change_team_user/1 returns a team_user changeset" do
-      team_user = team_user_fixture()
+      {:ok, %TeamUser{} = team_user} = team_user_fixture()
       assert %Ecto.Changeset{} = TeamContext.change_team_user(team_user)
     end
   end
